fb5e588d288c3f552425aeb455deeb90f26d4e65
alex <devkral@web.de>
2025-07-25 07:47:32 +0200

    fixes

--- intrusiondetection/main.py
+++ intrusiondetection/main.py
@@ -1,11 +1,18 @@
 #! python3
 # coding:utf-8
 import datetime
+import hmac
 import os
+import smtplib
 import statistics
 from collections import deque
+from contextlib import contextmanager
+
+# Import the email modules we'll need
+from email.message import EmailMessage
 from functools import partial
 from pathlib import Path
+from urllib.parse import urlsplit
 
 import cv2
 import numpy as np
@@ -29,6 +36,10 @@ class KivyCamera(Image):
         self.fps_counter = 0
         path = Path(self.config.get("reactions", "image_dir"))
         path.mkdir(parents=True, exist_ok=True)
+        email_server = self.config.get("reactions", "email_server").strip()
+        if email_server:
+            with self.create_smtp_context(timeout=4) as s:
+                s.noop()
         Clock.schedule_interval(self.update, 1.0 / self.fps)
         Clock.schedule_once(
             partial(self.set_alarm, state=True),
@@ -67,14 +78,80 @@ class KivyCamera(Image):
     def set_alarm_triggered_off(self, dt=None):
         self.alarm_triggered = False
 
+    @contextmanager
+    def create_smtp_context(self, timeout=60):
+        email_server = self.config.get("reactions", "email_server").strip()
+        email_url = urlsplit(email_server)
+        if email_url.scheme == "smtp":
+            smtp_class = smtplib.SMTP
+
+        else:
+            smtp_class = smtplib.SMTP_SSL
+
+        class SMTP_fixed(smtp_class):
+            def auth_cram_md5(self, challenge=None):
+                if challenge is None:
+                    return None
+                return (
+                    self.user
+                    + " "
+                    + hmac.HMAC(
+                        self.password.encode("utf8"), challenge, "md5"
+                    ).hexdigest()
+                )
+
+        try:
+            with SMTP_fixed(
+                email_url.hostname, port=email_url.port or 0, timeout=timeout
+            ) as s:
+                if email_url.scheme == "smtp":
+                    s.starttls()
+                if email_url.username and email_url.password:
+                    s.login(
+                        email_url.username,
+                        email_url.password,
+                    )
+                yield s
+        except TimeoutError as exc:
+            print(
+                "timeout for",
+                email_url.hostname,
+                email_url.port,
+                "original",
+                email_server,
+            )
+            raise exc
+
     def raise_alarm(self, dt, *, count):
-        if count == 0:
-            print("alarm activated")
+        now = datetime.datetime.now(tz=datetime.UTC)
         path = Path(self.config.get("reactions", "image_dir"))
+        full_path = f"{path}{os.sep}{now.isoformat()}.jpg"
         cv2.imwrite(
-            f"{path}{os.sep}{datetime.datetime.now(tz=datetime.UTC).isoformat()}.jpg",
+            full_path,
             self.last_real_frame,
         )
+        email_recipients = self.config.get("reactions", "email_recipients").strip()
+        email_server = self.config.get("reactions", "email_server").strip()
+        if email_recipients and email_server:
+            email_url = urlsplit(email_server)
+            msg = EmailMessage()
+            msg["Subject"] = f"Intrusion alert: {now}"
+            sender = self.config.get("reactions", "email_sender").strip()
+            if not sender:
+                sender = f"{email_url.username or 'intrusiondetection'}@{email_url.hostname.removeprefix('smtp.')}"
+            msg["From"] = sender
+            msg["To"] = email_recipients
+            msg.set_content(f"Intrusion alert triggered at {now}")
+            with open(full_path, "rb") as rob:
+                msg.add_attachment(
+                    rob.read(),
+                    maintype="image",
+                    subtype="jpg",
+                    filename=f"{now.isoformat()}.jpg",
+                )
+
+            with self.create_smtp_context() as s:
+                s.send_message(msg)
         if count < self.config.getint("alarm", "repeats"):
             Clock.schedule_once(
                 partial(self.raise_alarm, count=count + 1),
@@ -125,7 +202,15 @@ class IntrusionApp(App):
                 "cooldown": "10",
             },
         )
-        config.setdefaults("reactions", {"image_dir": "detections"})
+        config.setdefaults(
+            "reactions",
+            {
+                "image_dir": "detections",
+                "email_recipients": "",
+                "email_sender": "",
+                "email_server": "localhost",
+            },
+        )
 
     def build(self):
         config = self.config
